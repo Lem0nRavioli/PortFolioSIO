5a0bd6eb975b8ef1d084f13f76679a127d12bdc8
Guillaume Saulnier <hirona_mj@hotmail.fr>
2022-05-02 10:37:16 +0200

    update link projet

--- assets/vendor/typed.js/dropdown.js
+++ assets/vendor/typed.js/dropdown.js
@@ -1,23 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const btns = document.querySelectorAll(".acc-btn-droppy");
-  
-  
-    function accordion() {
-  
-      this.classList.toggle("is-open");
-    
-  
-      const content = this.nextElementSibling;
-    
-  
-      if (content.style.maxHeight) content.style.maxHeight = null;
-      // else content.style.maxHeight = "100%";
-      else content.style.maxHeight = 30000 + "px";
-      // else content.style.maxHeight = content.scrollHeight + "px";
-    }
-    
-    // event
-    btns.forEach((el) => el.addEventListener("click", accordion));
-    
-    
-    });
+  const btns = document.querySelectorAll(".acc-btn-droppy");
+
+
+  function accordion() {
+
+    this.classList.toggle("is-open");
+
+
+    const content = this.nextElementSibling;
+
+
+    if (content.style.maxHeight) content.style.maxHeight = null;
+    // else content.style.maxHeight = "100%";
+    else content.style.maxHeight = 30000 + "px";
+    // else content.style.maxHeight = content.scrollHeight + "px";
+  }
+
+  // event
+  btns.forEach((el) => el.addEventListener("click", accordion));
+
+
+});
+
